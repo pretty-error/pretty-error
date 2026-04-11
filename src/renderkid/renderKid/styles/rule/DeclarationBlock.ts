2ca2030732534813afb02694f9234ec25008c798
Roman <dev@rman.dev>
2026-04-11 14:31:06 +0100

    format

--- src/renderkid/renderKid/styles/rule/DeclarationBlock.ts
+++ src/renderkid/renderKid/styles/rule/DeclarationBlock.ts
@@ -60,7 +60,7 @@ const DeclarationBlock = function () {
           value: function set(prop, value) {
             let key, val;
 
-            if (typeof(prop) === "object") {
+            if (typeof prop === "object") {
               for (key in prop) {
                 val = prop[key];
                 this.set(key, val);
--- src/renderkid/renderKid/styles/rule/declarationBlock/_Length.ts
+++ src/renderkid/renderKid/styles/rule/declarationBlock/_Length.ts
@@ -52,7 +52,7 @@ function _createSuper(Derived) {
 }
 
 function _possibleConstructorReturn(self, call) {
-  if (call && (typeof(call) === "object" || typeof call === "function")) {
+  if (call && (typeof call === "object" || typeof call === "function")) {
     return call;
   }
   return _assertThisInitialized(self);
