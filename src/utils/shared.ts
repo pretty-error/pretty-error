1426a23d7fdaf1c6cedd5fb8fa177a61ef5bf32c
Roman <dev@rman.dev>
2026-04-11 19:53:58 +0100

    wip

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -9,4 +9,20 @@ const LARGE_ARRAY_SIZE = 200;
 /** Used to stand-in for `undefined` hash values. */
 const HASH_UNDEFINED = "__lodash_hash_undefined__";
 
-export { arrayProto, funcProto, objectProto, LARGE_ARRAY_SIZE, HASH_UNDEFINED };
+function isLength(value) {
+  return (
+    typeof value == "number" &&
+    value > -1 &&
+    value % 1 == 0 &&
+    value <= Number.MAX_SAFE_INTEGER
+  );
+}
+
+export {
+  arrayProto,
+  funcProto,
+  objectProto,
+  LARGE_ARRAY_SIZE,
+  HASH_UNDEFINED,
+  isLength,
+};
