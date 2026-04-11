0eb67c32f71ef68cea1f6011aae2f51cfbf837f0
Roman <dev@rman.dev>
2026-04-11 18:26:27 +0100

    wip

--- tests/pretty-error.test.ts
+++ tests/pretty-error.test.ts
@@ -40,7 +40,9 @@ function fixStupidGithubCI(str: string) {
 const sanitize = (str: string) => {
   return fixStupidGithubCI(
     fixStupidGithubCI(fixStupidGithubCI(Bun.stripANSI(stripFilePaths(str)))),
-  ).replace(/(\S) {4}(\S)/g, "$1$2");
+  )
+    .replace(/(\S) {4}(\S)/g, "$1$2")
+    .replace(/(\S) {5}(\S)/g, "$1 $2");
 };
 
 function snapshot(stack) {
