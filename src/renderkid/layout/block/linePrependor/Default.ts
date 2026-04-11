446e059494e55396f13e49b17ba7bce263a219c4
Roman <dev@rman.dev>
2026-04-11 21:27:20 +0100

    Update Default.ts

--- src/renderkid/layout/block/linePrependor/Default.ts
+++ src/renderkid/layout/block/linePrependor/Default.ts
@@ -1,63 +1,9 @@
-const _typeof = (obj) => typeof obj;
-
-import { _classCallCheck, _createClass } from "#renderkid/tools";
-
-function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function");
-  }
-  subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: { value: subClass, writable: true, configurable: true },
-  });
-  if (superClass) _setPrototypeOf(subClass, superClass);
-}
-
-const _setPrototypeOf = Object.setPrototypeOf;
-
-function _createSuper(Derived) {
-  const hasNativeReflectConstruct = _isNativeReflectConstruct();
-  return function _createSuperInternal() {
-    let Super = p(Derived),
-      result;
-    if (hasNativeReflectConstruct) {
-      const NewTarget = p(this).constructor;
-      result = Reflect.construct(Super, arguments, NewTarget);
-    } else {
-      result = Super.apply(this, arguments);
-    }
-    return _possibleConstructorReturn(this, result);
-  };
-}
-
-function _possibleConstructorReturn(self, call) {
-  if (call && (typeof call === "object" || typeof call === "function")) {
-    return call;
-  }
-  return _assertThisInitialized(self);
-}
-
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError(
-      "this hasn't been initialised - super() hasn't been called",
-    );
-  }
-  return self;
-}
-
-function _isNativeReflectConstruct() {
-  if (typeof Proxy === "function") return true;
-  try {
-    Boolean.prototype.valueOf.call(
-      Reflect.construct(Boolean, [], function () {}),
-    );
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-import { p } from "#utils";
+import {
+  _classCallCheck,
+  _createClass,
+  _createSuper,
+  _inherits,
+} from "#renderkid/tools";
 
 import SpecialString from "../../SpecialString";
 import _LinePrependor from "./_LinePrependor";
