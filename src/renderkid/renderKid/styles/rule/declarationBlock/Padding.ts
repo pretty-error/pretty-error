6f4edf4314363c62a67e7e8905f81ec8438f0ed0
Roman <dev@rman.dev>
2026-04-11 21:17:44 +0100

    wip

--- src/renderkid/renderKid/styles/rule/declarationBlock/Padding.ts
+++ src/renderkid/renderKid/styles/rule/declarationBlock/Padding.ts
@@ -6,51 +6,7 @@ import PaddingTop from "./PaddingTop";
 
 const _setPrototypeOf = Object.setPrototypeOf;
 
-function _createSuper(Derived) {
-  const hasNativeReflectConstruct = _isNativeReflectConstruct();
-  return function _createSuperInternal() {
-    let Super = p(Derived),
-      result;
-    if (hasNativeReflectConstruct) {
-      const NewTarget = p(this).constructor;
-      result = Reflect.construct(Super, arguments, NewTarget);
-    } else {
-      result = Super.apply(this, arguments);
-    }
-    return _possibleConstructorReturn(this, result);
-  };
-}
-
-function _possibleConstructorReturn(self, call) {
-  if (call && (typeof call === "object" || typeof call === "function")) {
-    return call;
-  }
-  return _assertThisInitialized(self);
-}
-
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError(
-      "this hasn't been initialised - super() hasn't been called",
-    );
-  }
-  return self;
-}
-
-function _isNativeReflectConstruct() {
-  if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-  if (Reflect.construct.sham) return false;
-  if (typeof Proxy === "function") return true;
-  try {
-    Boolean.prototype.valueOf.call(
-      Reflect.construct(Boolean, [], function () {}),
-    );
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
+import { _createSuper } from "#renderkid/tools";
 import { p } from "#utils";
 
 const Padding = function () {
